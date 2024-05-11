bb845f42092158ef4e7495c179e76ceff9f96ccc
Bohdan Matviiv <bomtvv@gmail.com>
2024-05-12 01:01:21 +0300

    make header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter as FontSans } from "next/font/google";
+import { Header } from "@/components/Header";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${fontSans.style.fontFamily};
+          @apply min-h-screen bg-background font-sans antialiased;
+        }
+      `}</style>
+
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
